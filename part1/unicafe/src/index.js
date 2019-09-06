9c757d01cfa19ddf29cb3ca2b16c0787610bfb54
Gustavo Pereira <gustavopereira233@gmail.com>
2019-09-06 13:29:18 -0300

    Finished Unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Title = props => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Button = props => {
+    return (
+        <button onClick={props.handleClick}>
+            {props.name}
+        </button>
+    );
+};
+
+const Stat = props => {
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{props.value}</td>
+        </tr>
+    );
+}
+
+const Statistics = ({feedback}) => {
+    const { good, neutral, bad } = feedback;
+    
+    const all = good + neutral + bad;
+    const average = (good * 1) + (bad * -1);
+    const percentage = (good / all) * 100;
+
+    if (all > 0) {
+        return (
+            <table>
+                <tbody>
+                    <Stat name='good' value={good} />
+                    <Stat name='neutral' value={neutral} />
+                    <Stat name='bad' value={bad} />
+                    <Stat name='all' value={all}/>
+                    <Stat name='average' value={average} />
+                    <Stat name='percentage' value={percentage}/>
+                </tbody>
+            </table>
+        );
+    } else {
+        return (
+            <div>
+                <p>No feedback given</p>
+            </div>
+        );
+    }
+}
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const feedback = {
+        good: good,
+        neutral: neutral,
+        bad: bad,
+    };
+
+    return (
+        <div>
+            <Title title="give feedback" />
+            <Button name="good" handleClick={()=> setGood(good + 1)}/>
+            <Button name="neutral" handleClick={()=> setNeutral(neutral + 1)}/>
+            <Button name="bad" handleClick={()=> setBad(bad + 1)}/>
+            <Title title="give statistics" />
+            <Statistics feedback={feedback}/>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
