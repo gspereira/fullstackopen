df87f7be02b650ab3dab655305a3cfe94e7ed4a5
Gustavo Pereira <gustavopereira233@gmail.com>
2019-09-11 08:57:09 -0300

    Phonebook stage 2 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,37 +1,42 @@
 import React, { useState } from "react";
 
 const App = props => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
-      ]);
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
     const [newName, setNewName] = useState("");
 
     const addPerson = event => {
         event.preventDefault();
-        const personObject = {
-            name: newName,
+        if (persons.filter(person => person.name === newName).length > 0) {
+            alert(`${newName} is already added to phonebook`);
+            setNewName("");
+        } else {
+            const personObject = {
+                name: newName
+            };
+            setPersons(persons.concat(personObject));
+            setNewName("");
         }
-        setPersons(persons.concat(personObject));
-        setNewName("");
-    }
+    };
 
     const handleNameChange = event => {
         setNewName(event.target.value);
-    }
+    };
 
     return (
         <div>
             <h2>Phonebook</h2>
             <form onSubmit={addPerson}>
                 <div>
-                    name: <input value={newName} onChange={handleNameChange}/>
+                    name: <input value={newName} onChange={handleNameChange} />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person => <p key={person.name}>{person.name}</p>)}
+            {persons.map(person => (
+                <p key={person.name}>{person.name}</p>
+            ))}
         </div>
     );
 };
